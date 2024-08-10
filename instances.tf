c07f508f00edc3befe526859caf178ac5d39ba5e
Sachin Rajvanshi <airan.sachinraj@gmail.com>
2024-08-10 17:22:13 +0530

    Add files via upload

--- instances.tf
+++ instances.tf
@@ -10,15 +10,12 @@ required_version = ">= 1.2.0"
 provider "aws" {
         region = "us-east-1"
 }
-locals {
-instances = ("Raj":"ami-08c40ec9ead489470","Vidit":"ami-08c40ec9ead489470","Rajvanshi":"0b0dcb5067f052a63","Gupta":"0b0dcb5067f052a63","Akhil":"0b0dcb5067f052a63")
-}
 resource "aws_instance" "my_instance" {
-        for_each = local_instances 
-        ami = each.key
+        count = 5
+        ami = "ami-08c40ec9ead489470"
         instance_type = "t2.micro"
         tags = {
-                Name = each.value
+                Name = "Batches"
 }
 }
 
