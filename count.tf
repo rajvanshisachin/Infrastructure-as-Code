93396f64a2101209afc1cf3bdeda687e3319ab99
Sachin Rajvanshi <airan.sachinraj@gmail.com>
2024-08-10 20:05:23 +0530

    Update count.tf

--- count.tf
+++ count.tf
@@ -1,24 +1,26 @@
 terraform {
-        required_providers {
-        aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.16"
-}
-}
-required_version = ">= 1.2.0"
-}
-provider "aws" {
-        region = "us-east-1"
-}
-resource "aws_instance" "my_instance" {
-        count = 5
-        ami = "ami-08c40ec9ead489470"
-        instance_type = "t2.micro"
-        tags = {
-                Name = "Batch- ${count.index}"
-}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
 }
 
-output "ec2_public_ip" {
-        value = aws_instance.my_instance[*].public_ip
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+  count = 5
+  ami           = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Batch-${count.index}"
+  }
+}
+
+output "batch_instance_public_ips" {
+  value = aws_instance.my_instance[*].public_ip
 }
