2ecf1b139cc7869a725b09894bcf955c121060ea
Sachin Rajvanshi <airan.sachinraj@gmail.com>
2024-08-10 16:46:32 +0530

    Update changes to instances.tf

--- instances.tf
+++ instances.tf
@@ -7,18 +7,20 @@ terraform {
 }
 required_version = ">= 1.2.0"
 }
-
 provider "aws" {
         region = "us-east-1"
-
 }
-
+locals {
+instance_names = toset(["Raj","Vidit","Rajvanshi","Gupta","Akhil"])
+}
 resource "aws_instance" "my_instance" {
-        count = 5
+       #count = 5
+        for_each = local_instances 
         ami = "ami-08c40ec9ead489470"
         instance_type = "t2.micro"
         tags = {
-                Name = "Batches"
+               #Name = "Batches"
+                Name = each.key
 }
 }
 
